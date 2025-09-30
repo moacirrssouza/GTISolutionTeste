65bc65cd3dead9c31654162a7a50a9cad2c9342c
moacirrssouza <moacir.rs.souza@outlook.com>
2025-09-30 12:58:43 -0300

    feat: criação do wfc e adicionando outras camadas do projeto

---
+++ ModeloProjeto/WCFServiceHost/Enums/EstadoCivil.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+
+namespace WCFServiceHost.Enums
+{
+    [DataContract]
+    public enum EstadoCivil
+    {
+        [EnumMember] Solteiro,
+        [EnumMember] Casado,
+        [EnumMember] Divorciado,
+        [EnumMember] Viuvo,
+        [EnumMember] Separado,
+        [EnumMember] UniaoEstavel
+    }
+}
---
+++ ModeloProjeto/WCFServiceHost/ICadastroService.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using WCFServiceHost.Enums;
+
+namespace WCFServiceHost
+{
+    [ServiceContract]
+    public interface ICadastroService
+    {
+        [OperationContract]
+        void CriarCliente(Cliente cliente);
+
+        [OperationContract]
+        Cliente BuscarClientePorId(int id);
+
+        [OperationContract]
+        void AlterarCliente(Cliente cliente);
+
+        [OperationContract]
+        void ExcluirCliente(int id);
+
+        [OperationContract]
+        List<Cliente> ListarClientes();
+    }
+
+    [DataContract]
+    public class Endereco
+    {
+
+        [DataMember]
+        public string CEP { get; set; }
+
+        [DataMember]
+        public string Logradouro { get; set; }
+
+        [DataMember]
+        public string Numero { get; set; }
+
+        [DataMember]
+        public string Complemento { get; set; }
+
+        [DataMember]
+        public string Bairro { get; set; }
+
+        [DataMember]
+        public string Cidade { get; set; }
+
+        [DataMember]
+        public UF UF { get; set; }
+    }
+
+    [DataContract]
+    public class Cliente
+    {
+
+        [DataMember]
+        public string CPF { get; set; }
+
+        [DataMember]
+        public string Nome { get; set; }
+
+        [DataMember]
+        public string RG { get; set; }
+
+        [DataMember]
+        public DateTime DataExpedicao { get; set; }
+
+        [DataMember]
+        public string OrgaoExpedicao { get; set; }
+
+        [DataMember]
+        public string UF { get; set; }
+
+        [DataMember]
+        public DateTime DataNascimento { get; set; }
+
+        [DataMember]
+        public string Sexo { get; set; }
+
+        [DataMember]
+        public EstadoCivil EstadoCivil { get; set; }
+
+        [DataMember]
+        public Endereco Endereco { get; set; }
+    }
+}
