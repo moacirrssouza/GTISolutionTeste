f841893599e794f8a3b9c8ef526c6ae8889570f4
moacirrssouza <moacir.rs.souza@outlook.com>
2025-10-02 20:11:32 -0300

    feat: criação da webmvc

---
+++ ModeloProjeto/WebMVC/Models/ClienteViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WebMVC.Models
+{
+    public class ClienteViewModel
+    {
+        public int Id { get; set; }
+        public string CPF { get; set; }
+        public string Nome { get; set; }
+        public string RG { get; set; }
+        public DateTime DataExpedicao { get; set; }
+        public string OrgaoExpedicao { get; set; }
+        public string UF { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Sexo { get; set; }
+        public string EstadoCivil { get; set; }
+        public virtual EnderecoViewModel Endereco { get; set; }
+        public int EnderecoId { get; set; }
+    }
+}
